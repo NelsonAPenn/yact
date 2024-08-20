e8a0d63a3254f6dcd9539e32d75cb981731b387e
Nelson Penn <nelsonapenn@protonmail.com>
2024-08-20 00:03:06 -0400

    Use clap for yact binary

--- src/bin/yact.rs
+++ src/bin/yact.rs
@@ -1,6 +1,20 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use yact::{pre_commit, BuiltinTransformer, ShellCommandTransformer, TransformerOptions};
 
+#[derive(Parser)]
+#[command(version, about)]
+pub struct Args {
+    /*
+     * TODO: add config file and configuration
+     * #[arg(short, long, value_name="CONFIG_FILE")]
+     * config: Option<PathBuf>,
+     */
+}
+
 pub fn main() {
+    let _cli = Args::parse();
     let config = [
         (
             "**/*.rs",
